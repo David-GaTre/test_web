e7d955377f45150d093ba56a5b1e3a069fe12307
David Garcia <gtdavid2013@gmail.com>
2022-03-16 02:09:23 -0600

    Vue exercise

---
+++ practica_vue/basic.js
@@ -0,0 +1,14 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello David!',
+        pags:[
+            { message:"primer pagina ok"  },
+            { message:"Segunda pagina Nok" },
+            { message:'Tercer pagina ok' },
+        ],
+        name: 'Desarrollo WEB',
+        dt: '15 mar 2022',
+        imag:'https://gacetamedica.com/wp-content/uploads/2021/02/GettyImages-1201463812.jpg'
+    }
+});
